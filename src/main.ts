843f7d3f2c5269806492661e93018aa3208c66ee
Rahul Yadav <kumarrahul997761@gmail.com>
2024-10-24 14:48:23 +0530

    added module, controller, dtos

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { Root } from './root.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(Root);
   await app.listen(3000);
 }
 bootstrap();
